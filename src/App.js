f2405f3e371336282a6ff278cf833604cb387182
Maria Anais Reyes <maria.anaisr@gmail.com>
2023-01-12 13:19:38 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import User from "./components/User"
+import Modal from "./components/Modal"
+import { useState, useEffect } from "react"
 
 function App() {
+  const [users, setUsers] = useState(null)
+  useEffect(()=>{
+    fetch("https://randomuser.me/api/?results=30")
+    .then(result => result.json())
+    .then(data => setUsers(data.results))
+    
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-indigo-400">
+      <User />
+      { users ? 
+        users.map(user => {
+          return <span key={user.id.value || 
+            user.phone
+             }>{user.name.first}</span>
+        })
+       : <span>cargando...</span> }
     </div>
   );
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,7 @@
+const Modal = () => {
+    return(
+        <div className="text-xs">Modal</div>
+    )
+}
+
+export default Modal;
