6e974df6af3971c10a072aedf55707f6c9531c03
panchoman32 <estebandido32@hotmail.com>
2023-01-12 20:20:17 -0700

    commit EM

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-
 import "./App.css";
-import User from "./components/User"
-import Modal from "./components/Modal"
-import { useState, useEffect } from "react"
+import User from "./components/User";
+import Modal from "./components/Modal";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [users, setUsers] = useState(null)
-  useEffect(()=>{
+  const [users, setUsers] = useState(null);
+  useEffect(() => {
     fetch("https://randomuser.me/api/?results=30")
-    .then(result => result.json())
-    .then(data => setUsers(data.results))
-    
-  },[])
+      .then((result) => result.json())
+      .then((data) => setUsers(data.results));
+  }, []);
+
+  const handleClick = (user) => {
+    console.log(user);
+  };
+
   return (
-    <div className="text-indigo-400">
-      <User />
-      { users ? 
-        users.map(user => {
-          return <span key={user.id.value || 
-            user.phone
-             }>{user.name.first}</span>
+    <div className="container">
+      {users ? (
+        users.map((user) => {
+          return (
+            <div key={user.id.value || user.phone}>
+              <User user={user} handleClick={handleClick} />
+            </div>
+          );
         })
-       : <span>cargando...</span> }
+      ) : (
+        <span>cargando...</span>
+      )}
     </div>
   );
 }
--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,19 @@
-const User = () => {
-    return(
-        <div>User</div>
-    )
-}
+import userEvent from "@testing-library/user-event";
+
+const User = ({ user, handleClick }) => {
+  return (
+    <div
+      onClick={() => {
+        handleClick(user);
+      }}
+    >
+      <img src={user.picture.medium}></img>
+      <span>
+        {user.name.title}.{user.name.first}
+        {user.name.last}
+      </span>
+    </div>
+  );
+};
 
 export default User;
