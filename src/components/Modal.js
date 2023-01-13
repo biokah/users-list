9cceb62cbd615baca91495902cb652c4d40358fb
Maria Anais Reyes <maria.anaisr@gmail.com>
2023-01-13 14:57:21 -0600

    adding toggle modal event to overlay

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,7 @@ import { MdClose } from "react-icons/md";
 const Modal = ({user, toggleModal}) => {
     return(
         <div className="fixed inset-0 w-full max-h-screen overflow-hidden modal-wrapper">
-            <div className="bg-gray-500 opacity-50 w-full h-full"></div>
+            <div className="bg-gray-500 opacity-50 w-full h-full" onClick={toggleModal}></div>
             <div className="modal top-1/2 left-1/2 bg-white text-black absolute t-50 l-50">
                 <button className="ml-auto block m-4" onClick={toggleModal}><MdClose/></button>
                 <article className="flex gap-2 justify-center flex-col p-10 text-center">
