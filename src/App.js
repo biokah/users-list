02d34337a587c55a69aa478911598957da3d3d88
yosassenav <mromerovanessa.soy@gmail.com>
2023-01-12 23:52:23 -0500

    first attempt

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
-
 import "./App.css";
-import User from "./components/User"
-import Modal from "./components/Modal"
-import { useState, useEffect } from "react"
+import User from "./components/User";
+import Modal from "./components/Modal";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [users, setUsers] = useState(null)
-  useEffect(()=>{
+  const [users, setUsers] = useState(null);
+  const [selectedUser, setSelectedUser] = useState(null);
+  useEffect(() => {
     fetch("https://randomuser.me/api/?results=30")
-    .then(result => result.json())
-    .then(data => setUsers(data.results))
-    
-  },[])
+      .then((result) => result.json())
+      .then((data) => setUsers(data.results));
+  }, []);
 
   const handleClick = (user) => {
-    console.log(user)
-  }
+    console.log(user);
+  };
 
-  const toggleModal = () => {
-    
-  }
-
+  const toggleModal = (evt) => {
+    console.log(evt.target.value);
+  };
 
   return (
     <main>
-      <Modal />
+      {selectedUser ? (
+        <Modal user={setSelectedUser(selectedUser)} toggleModal={toggleModal} />
+      ) : null}
+
       <div className="container-wrapper m-auto grid md:grid-cols-2 grid-cols-1 gap-7">
-        { users ? 
-          users.map(user => {
-            return <div key={user.id.value || 
-              user.phone
-              }>
-              <User user={user} handleClick={handleClick}/></div >
+        {users ? (
+          users.map((user) => {
+            return (
+              <div key={user.id.value || user.phone}>
+                <User user={user} handleClick={handleClick} />
+              </div>
+            );
           })
-        : <span>cargando...</span> }
+        ) : (
+          <span>cargando...</span>
+        )}
       </div>
     </main>
   );
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,19 +1,25 @@
-const Modal = () => {
-    return(
-        <div className="fixed inset-0 w-full max-h-screen overflow-hidden modal-wrapper">
-            <div className="modal top-1/2 left-1/2 bg-white text-black absolute t-50 l-50">
-                <button className="ml-auto block">x</button>
-                <article className="flex justify-center flex-col p-10">
-                    <img alt="modal" src="http://placekitten.com/300/200" />
-                    <span>Nombre completo</span>
-                    <span>Gender</span>
-                    <span>Email</span>
-                    <span>Telefono</span>
-                </article>
-            </div>
-        </div>
-        
-    )
-}
+const Modal = ({ user, toggleModal }) => {
+  return (
+    <div className="fixed inset-0 w-full max-h-screen overflow-hidden modal-wrapper">
+      <div
+        className="modal top-1/2 left-1/2 bg-white text-black absolute t-50 l-50"
+        // onClick={() => {
+        //   toggleModal();
+        // }}
+      >
+        <button className="ml-auto block" onClick={toggleModal()}>
+          x
+        </button>
+        <article className="flex justify-center flex-col p-10">
+          <img alt="modal" src="http://placekitten.com/300/200" />
+          <span>{user.name}</span>
+          <span>{user.gender}</span>
+          <span>{user.email}</span>
+          <span>{user.phone}</span>
+        </article>
+      </div>
+    </div>
+  );
+};
 
 export default Modal;
