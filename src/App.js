794b3de0c503f529380ec81ae5ce6b4a0d18038d
Maria Anais Reyes <maria.anaisr@gmail.com>
2023-01-13 21:04:30 -0600

    WIP filters

--- src/App.js
+++ src/App.js
@@ -7,13 +7,29 @@ import { useState, useEffect } from "react"
 function App() {
   const [users, setUsers] = useState(null)
   const [selectedUser, setSelectedUser] = useState(null)
+  const [string, setString] = useState("")
+  const [backupUsers, setBackupUsers] = useState([])
 
   useEffect(()=>{
     fetch("https://randomuser.me/api/?results=30")
     .then(result => result.json())
-    .then(data => setUsers(data.results))
-    
+    .then((data) => {
+      setUsers(data.results)
+      setBackupUsers(data.results)
+    }
+      )
   },[])
+
+  useEffect(()=>{
+    if(backupUsers && string) {
+      const filteredUsers = backupUsers.filter(user =>{
+        return user.name.first.toLowerCase().includes(string.toLowerCase()) || user.name.last.toLowerCase().includes(string.toLowerCase())
+      })
+      setUsers(filteredUsers)
+    } else if(backupUsers && !string) {
+      setUsers(backupUsers)
+    }
+  }, [string])
 
   const handleClick = (user) => {
     setSelectedUser(user)
@@ -23,11 +39,16 @@ function App() {
     setSelectedUser(null)
   }
 
+  const filterByName = (event) => {
+    setString(event.target.value)
+  }
+
   return (
     <main>
       {selectedUser && <Modal user={selectedUser} toggleModal={toggleModal} />}
       <div className="container-wrapper m-auto grid md:grid-cols-2 grid-cols-1 gap-7 pt-10">
-        <input type="text" placeholder="Search..." className="border border-solid border-gray-700 p-2 rounded" />
+        <input onChange={filterByName} type="text" placeholder="Search..." className="border border-solid border-gray-700 p-2 rounded" />
+        {string}
         <select className="border border-solid border-gray-700 py-2 rounded">
           <option value="">Please choose an option</option>
           <option value="female">female</option>
