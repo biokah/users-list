fb7e0aa0a739637bb1b7ebd8be014ad3c5d0e308
Maria Anais Reyes <maria.anaisr@gmail.com>
2023-01-13 21:17:24 -0600

    adding functionality for filters

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   const [selectedUser, setSelectedUser] = useState(null)
   const [string, setString] = useState("")
   const [backupUsers, setBackupUsers] = useState([])
+  const [gender, setGender] = useState("")
 
   useEffect(()=>{
     fetch("https://randomuser.me/api/?results=30")
@@ -31,6 +32,17 @@ function App() {
     }
   }, [string])
 
+  useEffect(()=>{
+    if(gender.length) {
+      const filteredUsers = backupUsers.filter(user => {
+        return user.gender === gender
+      })
+      setUsers(filteredUsers)
+    } else {
+      setUsers(backupUsers)
+    }
+  }, [gender])
+
   const handleClick = (user) => {
     setSelectedUser(user)
   }
@@ -43,13 +55,16 @@ function App() {
     setString(event.target.value)
   }
 
+  const filterByGender = (event) => {
+    setGender(event.target.value)
+  }
+
   return (
     <main>
       {selectedUser && <Modal user={selectedUser} toggleModal={toggleModal} />}
       <div className="container-wrapper m-auto grid md:grid-cols-2 grid-cols-1 gap-7 pt-10">
         <input onChange={filterByName} type="text" placeholder="Search..." className="border border-solid border-gray-700 p-2 rounded" />
-        {string}
-        <select className="border border-solid border-gray-700 py-2 rounded">
+        <select className="border border-solid border-gray-700 py-2 rounded" onChange={filterByGender}>
           <option value="">Please choose an option</option>
           <option value="female">female</option>
           <option value="male">male</option>
